c12ddfbbfb1e3505867b28fc924dd300e3011f7f
Zachary Koepf <koepfzm@gmail.com>
2022-12-07 09:37:35 -0500

    adding menubar component to several views

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <menu-bar/>
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
@@ -7,3 +8,17 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import MenuBar from './components/MenuBar.vue';
+
+export default {
+  components: {
+    MenuBar
+  }
+};
+</script>
+
+<style>
+
+</style>
