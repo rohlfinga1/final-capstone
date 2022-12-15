5930861d73e38c8bec535a348a29a2b5164e444b
rohlfinga1 <103110033+rohlfinga1@users.noreply.github.com>
2022-12-15 14:28:54 -0500

    Card logo in menu bar

--- vue/src/components/MenuBar.vue
+++ vue/src/components/MenuBar.vue
@@ -1,7 +1,14 @@
 <template>
   <header id="menubar">
     <div class="title">
-      <h2>Crash Cards</h2>
+      <div>
+        <h2>Crash Cards</h2>
+        <br/>
+      </div>
+      <div>
+        <div class="loader"></div>
+        <br/>
+      </div>
     </div>
     <nav>
       <ul>
@@ -38,6 +45,9 @@ export default {
 </script>
 
 <style>
+#menubar {
+  align-items: center;
+}
 header {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
@@ -52,15 +62,25 @@ header {
   flex-grow: 1;
 }
 .title {
+  font-weight: 600;
+  font-size: 20px;
+  color: #203159;
+  height: 50px;
+  width: 250px;
   align-items: center;
   color: #FFFDEB;
   margin-left: 20px;
+  margin-top: 0;
+  justify-content: space-between;
 }
+
 
 header div {
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
+  justify-content: space-between;
+  align-content: space-around
 }
 header nav {
   display: flex;
@@ -120,5 +140,88 @@ h2 {
   text-align: center;
   font-style: italic;
   color: #FFFDEB;
+  margin-top: 0;
+  font-size: 2rem;
+}
+.loader {
+  width: 48px;
+  height: 48px;
+  margin: auto;
+  position: relative;
+  animation-play-state: paused;
+}
+
+.loader:before {
+  content: '';
+  width: 48px;
+  height: 5px;
+  background: rgba(73, 85, 121, 0.5);
+  position: absolute;
+  top: 60px;
+  left: 0;
+  border-radius: 50%;
+  /* animation: shadow324 0.5s linear infinite; */
+  animation-name: shadow324;
+  animation-duration: 0.5s;
+  animation-iteration-count: infinite;
+  animation-play-state: paused;
+}
+
+.loader:after {
+  content: '';
+  width: 100%;
+  height: 100%;
+  background: skyblue;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 4px;
+  /* animation: jump7456 0.5s linear infinite; */
+  animation-name: jump7456;
+  animation-duration: 0.5s;
+  animation-iteration-count: infinite;
+  animation-play-state: paused;
+}
+
+@keyframes jump7456 {
+  15% {
+    border-bottom-right-radius: 3px;
+  }
+
+  25% {
+    transform: translateY(9px) rotate(22.5deg);
+  }
+
+  50% {
+    transform: translateY(18px) scale(1, .9) rotate(45deg);
+    border-bottom-right-radius: 40px;
+  }
+
+  75% {
+    transform: translateY(9px) rotate(67.5deg);
+  }
+
+  100% {
+    transform: translateY(0) rotate(90deg);
+  }
+}
+
+@keyframes shadow324 {
+
+  0%,
+    100% {
+    transform: scale(1, 1);
+  }
+
+  50% {
+    transform: scale(1.2, 1);
+  }
+}
+
+.loader:hover::after {
+  animation-play-state: running;
+}
+.loader:hover::before {
+  animation-play-state: running;
 }
 </style>
