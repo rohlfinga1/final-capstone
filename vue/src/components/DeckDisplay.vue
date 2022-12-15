fb462b12d06b23288e685c59911159cb47ad9a0f
rohlfinga1 <103110033+rohlfinga1@users.noreply.github.com>
2022-12-15 11:59:04 -0500

    fixed decks

--- vue/src/components/DeckDisplay.vue
+++ vue/src/components/DeckDisplay.vue
@@ -3,7 +3,7 @@
     <div class="deck" v-bind:key="deck.deckId" v-bind:deck="deck">
       <div class="details">
         <h2 class="title">{{deck.name}}</h2>
-        <span class="caption">{{deck.description}}</span>
+        <div class="caption">{{deck.description}}</div>
       </div>
     </div>
   </div>
@@ -21,7 +21,32 @@ export default {
 
 <style>
 .deck {
-  /* overflow: visible; */
+ width: 300px;
+ height: 300px;
+ background: #ffffff;
+ transition: box_shadow .25s ease-in;
+ display: flex;
+ justify-content: center;
+ text-align: center;
+ font-size: 20px;
+ /* line-height: 200px; */
+ border-radius: 20px;
+}
+
+.deck {
+ box-shadow: rgba(73,85,121,0.5) 5px 3px,
+  rgba(73,85,121,0.4) 10px 6px,
+  rgba(73,85,121,0.3) 15px 9px;
+}
+
+.deck:hover {
+ box-shadow: rgba(32,49,81,0.5) 5px 3px,
+  rgba(32,49,81,0.4) 10px 6px,
+  rgba(32,49,81,0.3) 15px 9px;
+}
+
+/* .deck {
+  /* overflow: visible;
   position: relative;
   width: 190px;
   height: 254px;
@@ -41,19 +66,19 @@ export default {
   background: #fff;
   transition: 0.5s;
   z-index: -99;
-}
+}*/
 
 .details {
-  /* position: absolute; */
-  left: 50px;
+  /* position: absolute;*/
+  /* left: 50px;
   right: 50px;
   bottom: 50px;
   top: 50px;
-  padding: 10px;
+  padding: 10px; */
   /* height: 60px; */
   text-align: center;
   text-transform: uppercase;
-}
+} 
 
 .decks {
   text-decoration: none;
@@ -64,19 +89,21 @@ export default {
   font-weight: 600;
   font-size: 20px;
   color: #203159;
+  height: 50px;
+  width: 250px;
+  margin-top: 50px;
 }
 
 .caption {
   font-weight: 500;
   font-size: 16px;
   color: #495579;
-  display: block;
   margin-top: 5px;
   font-family: sans-serif;
 }
 
 /*Hover*/
-.deck:hover .imgbox {
+/* .deck:hover .imgbox {
   bottom: 80px;
 }
 
@@ -87,5 +114,5 @@ export default {
 .deck:hover:after {
   transform: rotate(10deg);
   box-shadow: 0 2px 20px rgba(0, 0, 0, .2);
-}
+} */
 </style>
